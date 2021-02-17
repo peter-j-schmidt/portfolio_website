a728a04ee931955a86ae4850bae654b820fbac2d
Peter Schmidt <mlbp912@gmail.com>
2021-02-17 14:39:50 -0500

    Removed more unused code from application.py file.

--- application.py
+++ application.py
@@ -6,8 +6,6 @@ from flask import Flask, render_template, request, redirect, session, jsonify
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flask_session import Session
-
 import datetime
 
 # initialize the app
