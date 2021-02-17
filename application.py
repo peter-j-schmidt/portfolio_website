dfc7e9414fc7ac95cf55a187c3c47165408785f5
Peter Schmidt <mlbp912@gmail.com>
2021-02-17 14:37:54 -0500

    removed unused code from application.py file.

--- application.py
+++ application.py
@@ -5,7 +5,6 @@ from flask_mail import Mail, Message
 from flask import Flask, render_template, request, redirect, session, jsonify
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-from helpers import login_required
 
 from flask_session import Session
 
