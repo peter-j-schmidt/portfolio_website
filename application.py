432a4f3a974024dfbb67917b1edf338bbc16a7d7
Peter Schmidt <mlbp912@gmail.com>
2021-02-17 16:08:40 -0500

    Removed unused code.

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 import os
 import sqlite3
-from flask_mail import Mail, Message
 
 from flask import Flask, render_template, request, redirect, session, jsonify
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
@@ -13,19 +12,6 @@ app = Flask(__name__)
 
 # initialize the database
 # conn = sqlite3.connect('database.db')
-
-# create cursor object for database manipulation
-# db = conn.cursor()
-
-""" This section is for future development
-# Configure the mail parameters
-app.config['MAIL_SSERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 25
-app.config['MAIL_USERNAME'] = 'email@gmail.com'
-app.config['MAIL_PASSWORD'] = '*****'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-"""
 
 # Homepage displaying hero image and other website sections
 @app.route('/')
@@ -47,39 +33,6 @@ def projects():
 def other():
     return render_template("other.html")
 
-"""
-@app.route('/contact, methods=['GET', 'POST'])
-def contact():
-
-    if request.method == 'POST':
-        first_name = request.form['first']
-        last_name = request.form['last']
-        email = request.form['email']
-        message = request.form['message']
-
-        if not first_name or not first_name.strip():
-            error = 'First name is missing'
-        if not last_name or not last_name.strip():
-            error = 'Last name is missing'
-        if not email or not email.strip() or '@' not in email:
-            error = 'email is either missing or not valid'
-
-        msg = Message(message, sender=email, recipients=['email@gmail.com'])
-
-        mail.send(msg)
-
-        return render_template("thanks.html")
-    
-    elif request.method == 'GET':
-        return render_template("contact.html")
-        """
-
-"""
-# A route for the personal blog.
-@app.route('/blog')
-def blog():
-    pass
-"""
 
 if __name__ == '__main__':
     app.run()
