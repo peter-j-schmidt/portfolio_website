67086169030ea5864d02996190bf5820f66b168f
Peter Schmidt <mlbp912@gmail.com>
2021-03-17 16:11:51 -0400

    Updated project models and admin page.

--- portfolio_website/projects/models.py
+++ portfolio_website/projects/models.py
@@ -6,5 +6,9 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    image = models.FilePathField(path="/projects/images")
+    link = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.title
 
