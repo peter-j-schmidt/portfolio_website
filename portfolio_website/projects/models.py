5bb819d1d7e14df05787ab59b938ddf2682515ba
peter-j-schmidt <schmidt.peter.john@gmail.com>
2021-03-21 20:54:14 -0400

    Added photo for blog link.

--- portfolio_website/projects/models.py
+++ portfolio_website/projects/models.py
@@ -6,7 +6,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=60)
-    image = models.ImageField(upload_to="static/projects/images")
+    image = models.ImageField(upload_to="../static/projects/images")
     link = models.CharField(max_length=200, null=True)
 
     def __str__(self):
